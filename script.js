2915f4b38dc537d3a3760ff4a13f3c771de661d4
Salomão Faleta <faletas41@gmail.com>
2022-02-08 16:06:36 -0300

    atualizações1.4

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function addBola(){
     var bola = document.createElement("div");
     bola.setAttribute("class", "bola");
 
-    var p1 = Math.floor(Math.random() * 400);
-    var p2 = Math.floor(Math.random() * 500);
+    var p1 = Math.floor(Math.random() * 380);
+    var p2 = Math.floor(Math.random() * 480);
    
     bola.setAttribute("style", "left:"+p1+"px;  top:"+p2+"px;");
     bola.setAttribute("onclick", "estourar(this)");
