645825bc812723ba787bd41ed6a3aa4990c8e083
Salomão Faleta <faletas41@gmail.com>
2022-02-08 10:48:56 -0300

    atualizações1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function addBola(){
     var bola = document.createElement("div");
     bola.setAttribute("class", "bola");
 
-    var p1 = Math.floor(Math.random() * 450);
+    var p1 = Math.floor(Math.random() * 430);
     var p2 = Math.floor(Math.random() * 580);
    
     bola.setAttribute("style", "left:"+p1+"px;  top:"+p2+"px;");
